c96edc767dab221809ea57bead71555332ab8ef8
Michal Altair Valášek <michal.valasek@altairis.cz>
2020-04-18 23:21:25 +0200

    Initial commit

---
+++ TzList/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+
+namespace TzList {
+    class Program {
+        static void Main(string[] args) {
+            var systemZones = TimeZoneInfo.GetSystemTimeZones().Select(x => new { x.Id, x.DisplayName });
+            var maxIdLength = systemZones.Max(x => x.Id.Length);
+
+            foreach (var item in systemZones) {
+                Console.WriteLine(string.Join(string.Empty, item.Id.PadRight(maxIdLength+2), item.DisplayName));
+            }
+        }
+    }
+}
