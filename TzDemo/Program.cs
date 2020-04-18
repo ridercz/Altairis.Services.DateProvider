85c2b122a949d73551be50deeed4ae51616c6560
Michal Altair Valášek <michal.valasek@altairis.cz>
2020-04-19 01:36:49 +0200

    Add demo program

---
+++ TzDemo/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Altairis.Services.DateProvider;
+
+namespace TzDemo {
+    class Program {
+        static void Main(string[] args) {
+            PrintTime("LocalDateProvider", new LocalDateProvider());
+            PrintTime("UtcDateProvider", new UtcDateProvider());
+            PrintTime("FixedDateProvider(new DateTime(2000, 1, 1))", new FixedDateProvider(new DateTime(2000, 1, 1)));
+            PrintTime("TzConvertDateProvider(\"Central Europe Standard Time\")", new TzConvertDateProvider("Central Europe Standard Time"));
+            PrintTime("TzConvertDateProvider(\"China Standard Time\")", new TzConvertDateProvider("China Standard Time"));
+            Console.WriteLine("Values trimmed to seconds.");
+        }
+
+        static void PrintTime(string name, IDateProvider dateProvider) {
+            var dt = dateProvider.Now;
+            Console.WriteLine(name);
+            Console.WriteLine("  {0,-40:o} ({1})", dt, dt.Kind);
+            Console.WriteLine();
+        }
+    }
+}
